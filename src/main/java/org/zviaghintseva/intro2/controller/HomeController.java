8bfa31facdf74a1513635774b3c2782818235913
Anna27032000 <zv.annna00@gmail.com>
2020-10-30 17:37:17 +0300

    commit 6

--- src/main/java/org/zviaghintseva/intro2/controller/HomeController.java
+++ src/main/java/org/zviaghintseva/intro2/controller/HomeController.java
@@ -2,7 +2,10 @@ package org.zviaghintseva.intro2.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.zviaghintseva.intro2.implement.ReceiverImpl;
@@ -11,15 +14,17 @@ import org.zviaghintseva.intro2.implement.WirelessSenderImpl;
 import org.zviaghintseva.intro2.interfaces.Receiver;
 import org.zviaghintseva.intro2.interfaces.Sender;
 
-@RestController
+@Controller
 public class HomeController {
     private org.springframework.context.annotation.AnnotationConfigApplicationContext context;
 
     @RequestMapping(value = "/")
-    public String home(){
+    public String home(@RequestParam(value = "name", required = false, defaultValue = "stranger")String name,
+                       Model model){
         context=new AnnotationConfigApplicationContext(JavaConfig.class);
        Sender sender = context.getBean("wirelessSenderImpl", Sender.class);
-       return sender.sendMessage("Message from JavaConfig");
+       model.addAttribute("msg", sender.sendMessage("Hello "+name+"!"));
+       return "test";
 
     }
 }
