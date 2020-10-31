57e766cc497413d2a58d3a21c15945e14439001e
Anna27032000 <zv.annna00@gmail.com>
2020-11-01 01:18:06 +0300

    commit 8

--- src/main/java/org/zviaghintseva/intro2/controller/HomeController.java
+++ src/main/java/org/zviaghintseva/intro2/controller/HomeController.java
@@ -15,11 +15,14 @@ import org.zviaghintseva.intro2.implement.WirelessSenderImpl;
 import org.zviaghintseva.intro2.interfaces.Receiver;
 import org.zviaghintseva.intro2.interfaces.Sender;
 
+import java.util.Collection;
+import java.util.List;
+
 @Controller
 public class HomeController {
     private org.springframework.context.annotation.AnnotationConfigApplicationContext context;
 
-    @RequestMapping(value = "/{name}")
+      @RequestMapping(value = "/{name}")
     public String home(@PathVariable("name") String name,
                        Model model){
         context=new AnnotationConfigApplicationContext(JavaConfig.class);
@@ -27,5 +30,15 @@ public class HomeController {
        model.addAttribute("msg", sender.sendMessage("Hello "+name+"!"));
        return "test";
 
+    }
+    @RequestMapping(value = "/")
+    public String getUsers(Model model){
+        Collection<User> users = List.of(
+                new User("John", "Smith", "js@asd.com"),
+                new User("Mikle", "Johnon", "mj@asd.com")
+
+        );
+        model.addAttribute("users", users);
+        return "users";
     }
 }
